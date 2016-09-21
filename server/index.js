5cd6e521bdbd97ebc89efd3c4613e709321a5b2a
KATT <alex@dice.fm>
2016-09-21 21:59:22 +0100

    get rid of config stuff

--- server/index.js
+++ server/index.js
@@ -7,10 +7,15 @@ import WebpackDevServer from 'webpack-dev-server';
 import historyApiFallback from 'connect-history-api-fallback';
 import chalk from 'chalk';
 import webpackConfig from '../webpack.config';
-import config from './config/environment';
 import schema from './data/schema';
 
-if (config.env === 'development') {
+const {
+  NODE_ENV,
+  PORT,
+  GRAPHQL_PORT,
+} = process.env;
+
+if (NODE_ENV === 'development') {
   // Launch GraphQL
   const graphql = express();
   graphql.use('/', graphQLHTTP({
@@ -18,13 +23,13 @@ if (config.env === 'development') {
     pretty: true,
     schema
   }));
-  graphql.listen(config.graphql.port, () => console.log(chalk.green(`GraphQL is listening on port ${config.graphql.port}`)));
+  graphql.listen(GRAPHQL_PORT, () => console.log(chalk.green(`GraphQL is listening on port ${GRAPHQL_PORT}`)));
 
   // Launch Relay by using webpack.config.js
   const relayServer = new WebpackDevServer(webpack(webpackConfig), {
     contentBase: '/build/',
     proxy: {
-      '/graphql': `http://localhost:${config.graphql.port}`
+      '/graphql': `http://localhost:${GRAPHQL_PORT}`
     },
     stats: {
       colors: true
@@ -35,12 +40,12 @@ if (config.env === 'development') {
 
   // Serve static resources
   relayServer.use('/', express.static(path.join(__dirname, '../build')));
-  relayServer.listen(config.port, () => console.log(chalk.green(`Relay is listening on port ${config.port}`)));
-} else if (config.env === 'production') {
+  relayServer.listen(PORT, () => console.log(chalk.green(`Relay is listening on port ${PORT}`)));
+} else if (NODE_ENV === 'production') {
   // Launch Relay by creating a normal express server
   const relayServer = express();
   relayServer.use(historyApiFallback());
   relayServer.use('/', express.static(path.join(__dirname, '../build')));
   relayServer.use('/graphql', graphQLHTTP({ schema }));
-  relayServer.listen(config.port, () => console.log(chalk.green(`Relay is listening on port ${config.port}`)));
+  relayServer.listen(PORT, () => console.log(chalk.green(`Relay is listening on port ${PORT}`)));
 }
