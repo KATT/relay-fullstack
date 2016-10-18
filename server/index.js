efeaf175b55641c723fa8098e6f9f6c53d0f02bf
KATT <alex@dice.fm>
2016-10-18 18:43:51 +0100

    disable relay demo

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-console, no-shadow */
 import path from 'path';
-import webpack from 'webpack';
+// import webpack from 'webpack';
 import express from 'express';
 import graphQLHTTP from 'express-graphql';
-import WebpackDevServer from 'webpack-dev-server';
+// import WebpackDevServer from 'webpack-dev-server';
 import historyApiFallback from 'connect-history-api-fallback';
 import chalk from 'chalk';
-import webpackConfig from '../webpack.config';
+// import webpackConfig from '../webpack.config';
 import schema from './data/schema';
 
 const {
@@ -25,22 +25,22 @@ if (NODE_ENV === 'development') {
   }));
   graphql.listen(GRAPHQL_PORT, () => console.log(chalk.green(`GraphQL is listening on port ${GRAPHQL_PORT}`)));
 
-  // Launch Relay by using webpack.config.js
-  const relayServer = new WebpackDevServer(webpack(webpackConfig), {
-    contentBase: '/build/',
-    proxy: {
-      '/graphql': `http://localhost:${GRAPHQL_PORT}`
-    },
-    stats: {
-      colors: true
-    },
-    hot: true,
-    historyApiFallback: true
-  });
+  // // Launch Relay by using webpack.config.js
+  // const relayServer = new WebpackDevServer(webpack(webpackConfig), {
+  //   contentBase: '/build/',
+  //   proxy: {
+  //     '/graphql': `http://localhost:${GRAPHQL_PORT}`
+  //   },
+  //   stats: {
+  //     colors: true
+  //   },
+  //   hot: true,
+  //   historyApiFallback: true
+  // });
 
-  // Serve static resources
-  relayServer.use('/', express.static(path.join(__dirname, '../build')));
-  relayServer.listen(PORT, () => console.log(chalk.green(`Relay is listening on port ${PORT}`)));
+  // // Serve static resources
+  // relayServer.use('/', express.static(path.join(__dirname, '../build')));
+  // relayServer.listen(PORT, () => console.log(chalk.green(`Relay is listening on port ${PORT}`)));
 } else if (NODE_ENV === 'production') {
   // Launch Relay by creating a normal express server
   const relayServer = express();
